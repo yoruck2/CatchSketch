adeedb660227a23d4562caf1d12e27c52cbe8c91
yoruck2 <yoruck2@gmail.com>
2024-08-18 20:52:09 +0900

    💄 SignInView 구현

---
+++ CatchSketch/Feature/SignIn/SignInView.swift
@@ -0,0 +1,46 @@
+//
+//  SignInView.swift
+//  CatchSketch
+//
+//  Created by dopamint on 8/18/24.
+//
+
+import UIKit
+import SnapKit
+
+final class SignInView: BaseView {
+    
+    let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
+    let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
+    let signInButton = ConfirmButton(title: "로그인")
+    let signUpButton = ConfirmButton(title: "회원가입")
+    
+    override func configureLayout() {
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(signInButton)
+        addSubview(signUpButton)
+        
+        emailTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+        passwordTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(emailTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+        signInButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+        signUpButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(signInButton.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+        }
+    }
+}
+
