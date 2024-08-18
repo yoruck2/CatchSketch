d7929fa125880000b00d96efbecef89a85ce4bd7
yoruck2 <yoruck2@gmail.com>
2024-08-18 20:57:34 +0900

    ✨ TargetType 프로토콜 구현

--- CatchSketch/Util/Network/TargetType.swift
+++ CatchSketch/Util/Network/TargetType.swift
@@ -7,30 +7,45 @@
 
 import Foundation
 import Alamofire
-// MARK: 라우터 패턴 -
-//1. Router Enum 왜 만듬?
-//2. TargetType Protocol 굳이 왜 필요함?
-//3. URLRequestConvertible이 뭔디
-//4. asURLRequest가 하고 있는건 뭐야
 
 protocol TargetType: URLRequestConvertible {
     var baseURL: String { get }
     var method: HTTPMethod { get }
     var path: String { get }
-    var headers: [String: String] { get }
-    var parameters: String? { get }
-    var queryItems: [URLQueryItem]? { get }
+    var headers: HTTPHeaders { get }
+    var parameters: Parameters? { get }
+    var encoding: ParameterEncoding { get }
+//    var queryItems: [URLQueryItem]? { get }
     var body: Data? { get }
 }
 
 extension TargetType {
+    
+    var baseURL: String {
+        return APIAuth.catchSketchAPI.baseURL + "/v1"
+    }
+    
+    var encoding: ParameterEncoding {
+            switch method {
+            case .get:
+                return URLEncoding.default
+            default: 
+                return JSONEncoding.default
+            }
+        }
+    
     func asURLRequest() throws -> URLRequest {
         let url = try baseURL.asURL().appendingPathComponent(path)
+//        print(baseURL)
+//        print(url)
         var request = try URLRequest(url: url, method: method)
-        request.allHTTPHeaderFields = headers
-        request.httpBody = body
-        request.httpBody = parameters?.data(using: .utf8)
-        return request
+        request.headers = headers
+        if let body = body {
+            request.httpBody = body
+        }
+        return try encoding.encode(request, with: parameters)
+//        request.httpBody = parameters?.data(using: .utf8)
+//        return request
     }
 }
 
