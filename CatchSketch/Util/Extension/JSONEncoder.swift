8a744d32d699ba6f49b98118089ab51ed5282841
yoruck2 <yoruck2@gmail.com>
2024-08-18 20:58:52 +0900

    ✨ JSONENcoder 익스텐션 구현

---
+++ CatchSketch/Util/Extension/JSONEncoder.swift
@@ -0,0 +1,23 @@
+//
+//  JsonEncoder.swift
+//  CatchSketch
+//
+//  Created by dopamint on 8/16/24.
+//
+
+import Foundation
+
+extension JSONEncoder {
+    
+    static let jsonEncoder = JSONEncoder()
+    
+    static func encode<E: Encodable>(with query: E) throws -> Data {
+        do {
+            let result = try jsonEncoder.encode(query)
+            return result
+        } catch {
+            print("인코딩 실패: \(error)")
+            throw error
+        }
+    }
+}
