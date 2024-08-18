ea62167f864528d910d67aad8f11ee1d48147c03
yoruck2 <yoruck2@gmail.com>
2024-08-18 20:54:45 +0900

    ✨ Profile 모델 생성

---
+++ CatchSketch/model/RequestModel/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  CatchSketch
+//
+//  Created by dopamint on 8/17/24.
+//
+
+import Foundation
+
+struct Profile: Encodable {
+    
+    struct Edit: Encodable {
+        let nick: String?
+        let phoneNum: String?
+        let birthDay: String?
+        let profile: Data?
+    }
+}
