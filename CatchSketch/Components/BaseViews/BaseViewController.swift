d3449fb1cb154fce26fba96446e672451799a52b
yoruck2 <yoruck2@gmail.com>
2024-08-18 20:43:19 +0900

    ✨ BaseViews 생성
    BaseViewCollectionViewCell, BaseView, BaseViewController

---
+++ CatchSketch/Components/BaseViews/BaseViewController.swift
@@ -0,0 +1,48 @@
+//
+//  BaseViewController.swift
+//  CatchSketch
+//
+//  Created by dopamint on 7/22/24.
+//
+
+import UIKit
+
+class BaseViewController<RootView: UIView>: UIViewController {
+    
+    let rootView = RootView()
+    
+    init() {
+        super.init(nibName: .none, bundle: .none)
+    }
+    override func loadView() {
+        view = rootView
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "",
+                                                           style: .plain,
+                                                           target: nil,
+                                                           action: nil)
+    }
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.tintColor = .black
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+        configureNavigationBar()
+        configureViewController()
+    }
+    
+    func configureViewController() {}
+    func bind() {}
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
