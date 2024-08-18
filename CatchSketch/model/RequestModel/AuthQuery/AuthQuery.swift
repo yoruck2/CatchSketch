151d4aae884a288f2c88872d2a8e8b2e443ede04
yoruck2 <yoruck2@gmail.com>
2024-08-18 20:54:13 +0900

    ✨ 로그인 및 회원가입 등 인증에 필요한 AuthQuery 모델 생성

---
+++ CatchSketch/model/RequestModel/AuthQuery/AuthQuery.swift
@@ -0,0 +1,24 @@
+//
+//  LogInQuery.swift
+//  CatchSketch
+//
+//  Created by dopamint on 8/15/24.
+//
+
+import Foundation
+
+struct AuthQuery {
+    struct Join: Encodable {
+        let email: String
+        let password: String
+        let nick: String
+    }
+    struct EmailValidation: Encodable {
+        let email: String
+    }
+    struct LogIn: Encodable {
+        let email: String
+        let password: String
+    }
+}
+
