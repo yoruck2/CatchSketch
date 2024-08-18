d849f6e03be18565353a554fa34e96ebbfeefa22
yoruck2 <yoruck2@gmail.com>
2024-08-18 20:54:34 +0900

    ✨ Post 모델 생성

---
+++ CatchSketch/model/RequestModel/Post.swift
@@ -0,0 +1,32 @@
+//
+//  Post.swift
+//  CatchSketch
+//
+//  Created by dopamint on 8/16/24.
+//
+
+import Foundation
+
+struct Post: Encodable {
+    let product_id: String?
+    
+    let title: String?
+    let content: String?
+    let content1: String?
+    let content2: String?
+    let content3: String?
+    let content4: String?
+    let content5: String?
+    
+    let files: [String]?
+    
+    struct ImageUpload: Encodable {
+        let files: [Data]
+    }
+    
+    struct View: Encodable {
+        let product_id: String?
+        let next: String?
+        let limit: String?
+    }
+}
