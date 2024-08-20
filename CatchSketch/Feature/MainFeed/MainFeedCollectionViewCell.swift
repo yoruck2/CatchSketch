c483690c7114e65ddce5d3cb94599b8eb571b26c
yoruck2 <yoruck2@gmail.com>
2024-08-20 23:57:00 +0900

    ✨ MainFeedCollectionViewCell 구현

--- CatchSketch/Feature/MainFeed/MainFeedCollectionViewCell.swift
+++ CatchSketch/Feature/MainFeed/MainFeedCollectionViewCell.swift
@@ -5,4 +5,58 @@
 //  Created by dopamint on 8/19/24.
 //
 
-import Foundation
+import UIKit
+import Then
+
+final class MainFeedCollectionViewCell: BaseCollectionViewCell {
+    private let titleLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.font = .boldSystemFont(ofSize: 18)
+        $0.textColor = .white
+    }
+    
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    func configureUI() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.right.bottom.equalToSuperview().inset(10)
+            make.height.equalTo(30)
+        }
+        
+        contentView.layer.cornerRadius = 10
+        contentView.clipsToBounds = true
+        
+        // 그림자 추가
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 0.2
+        layer.masksToBounds = false
+    }
+    
+    func configure(with title: String) {
+        titleLabel.text = title
+        contentView.backgroundColor = .random()
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1),
+                       alpha: 1.0)
+    }
+}
+
+
