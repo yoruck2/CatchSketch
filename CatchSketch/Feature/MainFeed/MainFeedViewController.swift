a02f27b105f7620ab2d8e9693789ecdfa7eb60d9
yoruck2 <yoruck2@gmail.com>
2024-08-20 23:57:48 +0900

    🚧 MainFeedViewController 및 뷰모델 임시 구현

--- CatchSketch/Feature/MainFeed/MainFeedViewController.swift
+++ CatchSketch/Feature/MainFeed/MainFeedViewController.swift
@@ -5,4 +5,46 @@
 //  Created by dopamint on 8/19/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class MainFeedViewController: BaseViewController<MainFeedView> {
+    private let viewModel = MainFeedViewModel()
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rootView.mainFeedCollectionView.register(MainFeedCollectionViewCell.self, forCellWithReuseIdentifier: "MainFeedCell")
+        
+        bindViewModel()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let layout = rootView.mainFeedCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let firstItemOffset = layout.itemSize.height / 2 - rootView.mainFeedCollectionView.frame.size.height / 2
+            rootView.mainFeedCollectionView.setContentOffset(CGPoint(x: 0, y: -rootView.mainFeedCollectionView.contentInset.top + firstItemOffset+160), animated: true)
+        }
+    }
+    private func bindViewModel() {
+        viewModel.items
+            .bind(to: rootView.mainFeedCollectionView.rx.items(cellIdentifier: "MainFeedCell", cellType: MainFeedCollectionViewCell.self)) { (row, element, cell) in
+                cell.configure(with: element)
+            }
+            .disposed(by: disposeBag)
+        
+        rootView.mainFeedCollectionView.rx.modelSelected(String.self)
+            .subscribe(onNext: { [weak self] item in
+                self?.showAlert(with: item)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func showAlert(with item: String) {
+        let alert = UIAlertController(title: "눌림", message: item, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
--- CatchSketch/Feature/MainFeed/MainFeedViewModel.swift
+++ CatchSketch/Feature/MainFeed/MainFeedViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class MainFeedViewModel {
+    
+    let items = BehaviorRelay<[String]>(value: [])
+    
+    init() {
+        // 더미 데이터 생성
+        let dummyData = (1...20).map { "Item \($0)" }
+        items.accept(dummyData)
+    }
+    struct Input {
+        let viewDidLoadTrigger: PublishSubject<Void>
+    }
+    
+    struct Output {
+        let loginResult: Observable<Result<LogInResponse, Error>>
+    }
+    
+    func transform(input: Input) -> Output {
+        let result = input.viewDidLoadTrigger
+            .map { query in
+                NetworkService.shared.logIn(query: .post(.postView()))
+            }
+            .catch { error in
+                //                    if (error as! APIError) == .expiredRefreshToken {
+                //                        return .just(.failure(error as! APIError))
+                //                    }
+                return .just(.failure(error))
+            }
+            .share(replay: 1)
+        
+        return Output(loginResult: result)
+    }
+}
