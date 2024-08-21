b112c0be9741cfc570af17a29d07d35456649807
yoruck2 <yoruck2@gmail.com>
2024-08-21 23:48:33 +0900

    ✨ PostResponse 모델 추가

--- CatchSketch/model/ResponseModel/PostResponse.swift
+++ CatchSketch/model/ResponseModel/PostResponse.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct PostResponse: Decodable {
+    let post_id: String
+    let product_id: String?
+    let title: String?
+    let price: String?
+//    let asdf: Bool
+    let content: String?
+    let content1: String?
+    let content2: String?
+    let content3: String?
+    let content4: String?
+    let content5: String?
+    let createdAt: Creator
+    let creator: String
+      
+    let files: [String]
+    let likes: [String]
+    let likes2: [String]
+    let hashTags: [String]
+    let comments: [String]
+}
+
+struct Creator: Decodable {
+    let user_id: String
+    let nick: String
+    let profileImage: String?
+}
+
