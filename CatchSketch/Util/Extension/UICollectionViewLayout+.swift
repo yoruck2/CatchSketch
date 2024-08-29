f1cbe493bed9f861459dab844215c85d86fa2c32
yoruck2 <yoruck2@gmail.com>
2024-08-29 23:59:24 +0900

    ✨ UICollectionViewLayout 익스텐션 추가

--- CatchSketch/Util/Extension/UICollectionViewLayout+.swift
+++ CatchSketch/Util/Extension/UICollectionViewLayout+.swift
@@ -5,4 +5,25 @@
 //  Created by dopamint on 8/29/24.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionViewLayout {
+    static var commentCollectionViewLayout: UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { _, _ in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .estimated(50))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .estimated(50))
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            section.interGroupSpacing = 10
+            
+            return section
+        }
+        return layout
+    }
+}
