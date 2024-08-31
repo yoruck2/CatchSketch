57badbe706770cebec7b7215cdce8d3b316498a4
yoruck2 <yoruck2@gmail.com>
2024-08-31 23:59:41 +0900

    🩹 Comment, Creator Codable 채택

--- CatchSketch/model/ResponseModel/PostResponse.swift
+++ CatchSketch/model/ResponseModel/PostResponse.swift
@@ -32,14 +32,14 @@ struct Post: Decodable {
     let comments: [Comment]?
 }
 
-struct Comment: Decodable {
+struct Comment: Codable {
     let comment_id: String?
     let content: String?
     let createdAt: String?
     let creator: Creator?
 }
 
-struct Creator: Decodable {
+struct Creator: Codable {
     let user_id: String?
     let nick: String?
     let profileImage: String?
